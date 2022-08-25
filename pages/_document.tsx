207b6b1a6e46e89633f7bd3747a2952c65830d9d
Ronit <ronitkhatri44@gmail.com>
2022-08-25 16:25:46 +0530

    adding files

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+// Trick to force dark theme
+
+import {Html, Head, Main, NextScript} from 'next/document'
+
+export default function Document() {
+
+    return(
+        <Html className='dark'>
+        <Head>
+        </Head>
+        <body>
+            <Main/>
+            <NextScript/>
+        </body>
+        </Html>
+    )
+}
